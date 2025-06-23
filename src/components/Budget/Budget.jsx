cda08d56309080d082c4dd8969b2d5fad1099046
Gerald Planty <plantygerald02@gmail.com>
2025-06-23 18:51:28 -0400

    Layout

--- src/components/Budget/Budget.jsx
+++ src/components/Budget/Budget.jsx
@@ -5,10 +5,26 @@ function Budget() {
     <section className='budget'>
       <div className='budget__header'>
         <h2 className='budget__title'>Budget</h2>
+        <p className='budget__net-income'>Net: $0</p>
+        <button className='budget__add-item'>Add income/expense</button>
       </div>
       <div className='budget__lists'>
-        <ul className='budget__income'>Income</ul>
-        <ul className='budget__expenses'>Expenses</ul>
+        <ul className='budget__list'>
+          <span className='budget__list-title'>Income</span>
+          <li className='budget__list-item'>
+            <p>Work</p>
+            <p className='budget__income'>+$500</p>
+            <button className='budget__edit-btn'></button>
+          </li>
+        </ul>
+        <ul className='budget__list'>
+          <span className='budget__list-title'>Expenses</span>
+          <li className='budget__list-item'>
+            <p>Utility</p>
+            <p className='budget__expense'>-$130</p>
+            <button className='budget__edit-btn'></button>
+          </li>
+        </ul>
       </div>
     </section>
   );
